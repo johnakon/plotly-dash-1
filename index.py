24376ab55d37b87f4ffe29c99b4b2eacf8145bcf
unknown <john.akonya@gmail.com>
2020-12-20 01:34:23 +0300

    Added bootstrap

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 # Connect to main app.py file
@@ -10,14 +11,25 @@ from app import server
 from apps import vgames, global_sales
 
 
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div([
-        dcc.Link('Video Games|', href='/apps/vgames'),
-        dcc.Link('Other Products', href='/apps/global_sales'),
-    ], className="row"),
-    html.Div(id='page-content', children=[])
-])
+app.layout = dbc.Container([
+                dbc.Row([
+                    dbc.Col(html.H3("WELCOME TO MY DASH BOARD",
+                            className="text-center text-primary, mb-4"),
+                        width=12, style={"textDecoration": "underline", "color":"teal"})
+                ]),
+                html.Div([
+                    dcc.Location(id='url', refresh=False),
+                    html.Div([
+                        html.H4(dcc.Link('Video Games|', href='/apps/vgames',
+                            style={"textDecoration": "none"}
+                        )),
+                        html.H4(dcc.Link('Other Products', href='/apps/global_sales',
+                            style={"textDecoration": "none"}
+                        )),
+                    ], className="row"),
+                    html.Div(id='page-content', children=[])
+                ])
+            ])
 
 
 @app.callback(Output('page-content', 'children'),
